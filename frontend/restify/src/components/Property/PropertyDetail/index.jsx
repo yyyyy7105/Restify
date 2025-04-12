78358af8e74f91477c50bb4a0a1612e6905d852e
yy-7105 <leoyang105@gmail.com>
2025-04-12 21:49:26 +0800

    update urls in frontend code

--- frontend/restify/src/components/Property/PropertyDetail/index.jsx
+++ frontend/restify/src/components/Property/PropertyDetail/index.jsx
@@ -46,7 +46,7 @@ function RevCreationForm() {
       event.target.elements["end-date"].value + "T12:00:00Z"
     );
 
-    fetch(`http://127.0.0.1:8000/revs/create/${prop.id}/`, {
+    fetch(`${process.env.REACT_APP_API_URL}:8000/revs/create/${prop.id}/`, {
       method: "POST",
       headers: { Authorization: `Bearer ` + localStorage.getItem("access") },
       body: formData,
